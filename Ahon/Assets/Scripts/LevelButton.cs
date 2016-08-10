e82dabc095ebde6255fe8ea2d8e06991a1adf394
Yeng Oliva <fmoliva27@gmail.com>
2016-08-10 22:44:16 +0800

    folder content
    Resource, prefab, Scripts, Scenes

---
+++ Ahon/Assets/Scripts/LevelButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour {
+
+	public Text LevelText;
+	public int unlocked;
+	public GameObject Star1;
+	public GameObject Star2;
+	public GameObject Star3;
+
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Ahon/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class LevelManager : MonoBehaviour {
+
+	[System.Serializable]
+	public class Level{
+		public string LevelText;
+		public int Unlocked;
+		public bool IsInteractable;
+
+		public Button.ButtonClickedEvent OnClickEvent;
+	}
+	public GameObject levelButton;
+	public Transform Spacer;
+	public List<Level> LevelList;
+
+
+
+	// Use this for initialization
+	void Start () {
+		FillList ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FillList(){
+		foreach (var level in LevelList) {
+			GameObject newbutton = Instantiate(levelButton) as GameObject;
+			LevelButton button = newbutton.GetComponent<LevelButton>();
+			button.LevelText.text = level.LevelText;
+
+			newbutton.transform.SetParent(Spacer);
+		}
+	}
+}
