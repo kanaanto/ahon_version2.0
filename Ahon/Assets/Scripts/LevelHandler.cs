4cfe7d53dbc6ecdc741af5c40571e3e32a4ffbc5
Yeng Oliva <fmoliva27@gmail.com>
2016-08-20 10:14:16 +0800

    Changed Script name of Level Manager plus Gameover panel

--- Ahon/Assets/Scripts/LevelHandler.cs
+++ Ahon/Assets/Scripts/LevelHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
-public class LevelManager : MonoBehaviour {
+public class LevelHandler : MonoBehaviour {
 	
 	[System.Serializable]
 	public class Level{
