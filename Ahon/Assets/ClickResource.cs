e2b7bf1e9abaad2ea5f15cfc1880ff9b10e82378
elisacruzph <elisacruzph@gmail.com>
2016-10-03 00:16:26 +0800

    Added Click Function to Level1 Resources (Build, Plant)

---
+++ Ahon/Assets/ClickResource.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Behaviour
+{
+	public class ClickResource : MonoBehaviour {
+		
+		public Button buildButton, plantButton;
+
+		private int currentBuildIndex = 0;
+		private int currentPlantIndex = 0;
+		
+		[SerializeField]
+		GameObject[] build;
+		
+		[SerializeField]
+		GameObject[] plant;
+		
+		public void Build()
+		{
+			currentBuildIndex = (currentBuildIndex + 1) % build.Length;
+			//currentBuildIndex += 1;
+			//currentBuildIndex++;
+			build[currentBuildIndex].SetActive(true);
+
+			if (currentBuildIndex == build.Length)
+			{
+				buildButton.interactable = false;
+			}
+		}
+		
+		public void Plant()
+		{
+			currentPlantIndex = (currentPlantIndex + 1) % plant.Length;
+			//currentPlantIndex += 1;
+			//currentPlantIndex++;
+			plant[currentPlantIndex].SetActive(true);
+
+			if (currentPlantIndex == plant.Length)
+			{
+				plantButton.interactable = false;
+			}
+		}
+	}
+}
+
